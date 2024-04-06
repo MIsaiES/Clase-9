dbb5930f15a7c28d16f78ec97f37cb49259b7bd8
umg <umg@JUT-LAB-1I3>
2024-04-06 10:57:54 -0600

    Agregar archivos de proyecto.

---
+++ Clase #9/Clases/carro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase__9.Clases
+{
+    public  class carro
+    {
+        public string marca { get;}
+        public int modelo { get;}
+        public string color { get; set;}
+        public string dueño { get; set;}
+        private int encendido = 0;
+        private int velocidad = 0;
+        public int velmax { get; set;}
+       
+        public int Encender(int e)
+        {
+            encendido = e;
+            return encendido;
+        }
+        public carro(string m, int mod , string col)
+        {
+            marca = m;
+            modelo = mod;
+            color = col;    
+        }
+      
+        public int aceleracion()
+        {
+            
+            velocidad += 15;
+            return velocidad;
+        }
+    
+    }
+
+    
+}//acciones que debemos hacer se llamas constructores (constructor)
---
+++ Clase #9/Program.cs
@@ -0,0 +1,23 @@
+﻿using Clase__9.Clases;
+int op;
+
+carro carro = new carro("Toyota", 2017, "Negro");
+carro carro2 = new carro("Nissan", 2021, "Rojo");
+carro.dueño = "Mario";
+carro.velmax= 180;
+
+Console.WriteLine("¿Desea encender el carro?\n1.Si\n0.No");
+int.TryParse(Console.ReadLine(), out op);
+carro.Encender(op);
+
+
+Console.WriteLine(carro.aceleracion() + " Km/H");
+Console.WriteLine(carro.aceleracion() + " Km/H");
+Console.WriteLine(carro.aceleracion() + " Km/H");
+
+
+Console.WriteLine($"El carro es de marca {carro.marca}");
+Console.ReadLine();
+
+Console.WriteLine($"El segundo carro es de marca {carro2.marca}");
+Console.ReadLine();
